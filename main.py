e2c1b499c958f4f4714f44ecf6a9d62193e82155
mrinsaf <mr.insaf@list.ru>
2024-12-26 15:47:20 +0300

    исправил начальное состояние проекта

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
+import logging
+
+from misc import create_kb
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Вывод информации при запуске бота
+logging.info("Запуск бота...")
+
+import asyncio
+import sys
+import traceback
+
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import Command
+from aiogram.methods import EditMessageText
+from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.context import FSMContext
+from aiogram.types import InlineKeyboardButton, KeyboardButton, InlineKeyboardMarkup, CallbackQuery, ReplyKeyboardMarkup
+from aiogram.types.web_app_info import WebAppInfo
+from aiogram import F
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.methods.send_message import SendMessage
+from aiogram.types import FSInputFile
+import sqlite3
+from datetime import date
+
+
+import sys
+import os
+
+
 TEST_TOKEN = "7373519023:AAGHAMx01azAa4XRtG_QP3JApqLLEcxFsSg"
-MAIN_TOKEN = '7129795991:AAHb793O24B1UvI0c-TlGQ-m_e1zDFM0x08'
+MAIN_TOKEN = '7313284335:AAEZpuTjVjdNLquAPcoWSvIck2T9HMb6Fek'
 
 dp = Dispatcher()
 
 
 @dp.message(Command('start'))
 async def start_command(message: types.Message, state: FSMContext):
-    kb = start_logic()
+    kb = create_kb()
     await message.answer("Выберите действие", reply_markup=kb.as_markup())
-
-
-@dp.callback_query(F.data == "back", CalculatePrice.start)
-async def start_command(callback: CallbackQuery, state: FSMContext):
-    kb = start_logic()
-    await callback.message.answer("Выберите действие", reply_markup=kb.as_markup())
-
-
-
 
 
 
